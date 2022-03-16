9a391a5fc3ab37359542e58a67f7520ad10ec52f
Thomas <thomas@sorza.com>
2022-03-16 23:50:26 +0100

    Added files: holaMundo.py and index.py

---
+++ intro-python/index.py
@@ -0,0 +1,58 @@
+#Esto es un comentario
+if 3 > 5:#Esto es un comentario
+    print('Esto no se va a imprimir')
+
+#if 5 > 3:#Acá va otro comentario
+ #   print('5 es mayor a 3')
+
+x = 5
+y = 'chachito feliz'
+
+#print(x, y)
+
+email = 'thomas@correo.com'
+#print(email)
+
+#Can't start with number
+_mi_var = 'chanchito'
+_miVar = 'chanchitos'
+MIVAR = 'CHANCHITo'
+a,b,c = 'lala', 'lele', 'lili'
+#print(a, b, c)
+
+valor1 = valor2 = valor3 = 'canchito_feliz'
+#print(valor1, valor2, valor3)
+
+#Concatenación
+inicio = 'Hola '
+final = 'mundo'
+#print(inicio + final)
+
+#Is diferent that use the , (the coma adds a space)
+#inicio = 'Hola '
+#final = 'mundo'
+#print(inicio, final)
+
+#palabra = 'hola mundo' #String
+oracion = "hola mundo comilla doble" #String
+
+entero = 20 #integer
+conDecimales = 20.2 #float
+complejo = 1j #complejos
+
+#print(palabra, oracion, entero, conDecimales, complejo)
+
+lista = ['hola','mundo','chanchito feliz']
+Array1 = [4,4,5]
+lista2 = lista.copy()
+lista2.append(4)
+lista.append(5)
+#lista.clear()
+#print(lista, lista2.count(2), Array1.count(4))
+#print(len(lista), len(lista2))
+largolista = len(lista) 
+largolista2 = len(lista2)
+
+#print(largolista, largolista2)
+
+print(lista[2])
