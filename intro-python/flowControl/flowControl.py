b50a02f35436e026b37c94236adad362ad6cff6c
Thomas <sthx09567541@gmail.com>
2023-02-08 09:27:45 -0500

    Compleated flow Control demo

--- intro-python/flowControl/flowControl.py
+++ intro-python/flowControl/flowControl.py
@@ -1,1 +1,44 @@
-# Flsw control Exercises
+# Flow control DEMO
+
+""" if 2 < 5:
+    print('2 is less than 5')"""
+# a == b
+# a < b
+# a > b
+# a != b
+# a <= b
+# a >= b
+
+""" if 2 == 2:
+    print('2 is equal 2')
+
+if 2 == 3:
+    print('3 is equal 2')
+
+if 2 > 5:
+    print('2 is greather than 5') """
+
+""" 
+if(2 < 5):
+    print ('2 is less than 5')
+elif(2 > 5):
+    print ('2 is greater than 5')
+else:
+    print ('2 is equal 5') """
+
+#Single line if
+
+if 3 == 3: print ('3 is equal 3')
+
+#ternary operator
+print('this is true') if 5 > 2 else print ('this is false') 
+
+#and & or
+if 3 > 2 and 3 < 5:
+    print("both are true")
+
+if 3 > 2 and 3 > 3:
+    print('one of the two or both conditions are false')
+
+if ((1<0) or (1>0)):
+    print ('one of the two or both conditions are true')
