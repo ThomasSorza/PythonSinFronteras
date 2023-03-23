b2954bc04c14ea5992a6b0e25d71e144a827ac23
Thomas <sthx09567541@gmail.com>
2023-03-23 17:24:04 -0500

    added methods & working herarchi

--- intro-python/POO/objects.py
+++ intro-python/POO/objects.py
@@ -4,10 +4,29 @@ class User:
     def __init__(self, name, lastName):
         self.name = name
         self.lastName = lastName
+    
+    #not necessary to have self but it's recommended
+    def greeting(self):
+        print('Hi, My name is ', self.name, self.lastName)
+
+class Admin(User):
+    def adminGreeting(self):
+        print('Hi, My name is ', self.name, 'I am the admin.')
 
 #here we create a user called user
 user = User('Thomas', 'Sorza')
-user2 = User('Jean', 'Sorza')
+
 
 # print(user) #Memory direction
-print(user.name, user.lastName, user2.name, user2.lastName)
+print(user.name, user.lastName)
+user.greeting()
+user.name = 'Arturo'
+user.greeting()
+""" del user.name
+# user.greeting()
+del user
+print(user) """
+
+admin = Admin('John','Ripper')
+admin.greeting()
+admin.adminGreeting()
