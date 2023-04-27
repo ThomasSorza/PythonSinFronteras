293e8e2dd85907664735cb5523647bcb3f2368b6
Thomas <sthx09567541@gmail.com>
2023-04-26 23:06:05 -0500

    added conditionals and format strings

---
+++ intro-python/dataStructure/conditions.py
@@ -0,0 +1,20 @@
+x = 10
+y = 3
+if x > y:
+    print(f'{x} is greater than {y}')
+
+sports_list = ['baseball', 'basket', 'football', 'hockey']
+
+if 'dodgeball' in sports_list:
+    print('dodgeball is on the sport list')
+else:
+    print('dodgeball is not on the sport list')
+
+n = int(input("Enter a number: "))
+
+if n > 0:
+    print("The number is positive")
+elif n == 0:
+    print("the number is cero")
+else:
+    print("the number is negative")
